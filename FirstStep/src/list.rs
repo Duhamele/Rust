92747c5db6d62008d25228acb0fc3f9c58450b86
Elie Duhamel <elie.duhamel@grenoble-inp.org>
2024-12-12 21:06:48 +0100

    Impossible de mut deux tranche  du même tableau en même temps

--- FirstStep/src/list.rs
+++ FirstStep/src/list.rs
@@ -36,13 +36,51 @@ fn fussion(droit_tableau:& [u32],gauche_tableau:&[u32],arrive_tableau:&mut[u32])
     }
 
 }
+
+fn sort_fussion(tableau:&mut [u32],tableau_libre:&mut [u32]){
+    if tableau.len()<3{
+        if tableau.len()==2{
+            if tableau[0]>tableau[1]{
+                let valeur=tableau[0];
+                tableau[0]=tableau[1];
+                tableau[1]=valeur
+            }
+        }
+    }
+
+}
 fn bulle(tableau:&mut [u32]){
     for i in 1..tableau.len(){
-        let valeur:u32;
+        let valeur:u32=tableau[i];
+        let mut ind=i;
+        while tableau[ind-1]>valeur {
+            tableau[ind]=tableau[ind-1];
+            ind-=1;
+            if ind==0{
+                break;
+            }
+        }
+        tableau[ind]=valeur;
     }
 }
-
-
+#[test]
+fn test_bulle(){
+    let mut tableau=[5,9,1,2,7,8,3,4,0,6];
+    bulle(&mut tableau);
+    if tableau!=[0,1,2,3,4,5,6,7,8,9]{
+        assert!(true);
+    }
+    
+}
+#[test]
+fn test_fussion(){
+    let mut tableau=[2,3,5,7,8,0,1,4,6,9];
+    let mut results:[u32;10]=[0,0,0,0,0,0,0,0,0,0];
+    fussion(&mut tableau[0..5], &mut tableau[5..10],&mut results);
+    if results!=[0,1,2,3,4,5,6,7,8,9]{
+        assert!(true);
+    }
+}
 
 
 
